dad12ba2873eb5275da79f041178fdac61594466
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2025-07-26 19:46:57 +0530

    Adding clerk route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 // Initialize express
 const app = express();
@@ -16,7 +17,7 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send("Backend is working");
 })
-app.post('/clerk', express.json(), )
+app.post('/clerk', express.json(), clerkWebhooks)
 
 // Port
 const PORT = process.env.PORT || 5000;
